bf6a38ad25a5224c415746be8887762d2e04cba3
Manjunath Aithal <manjunathaithal19@gmail.com>
2020-12-04 01:33:44 +0530

    made changes

--- src/components/index.js
+++ src/components/index.js
@@ -6,8 +6,80 @@
 //      children: []
 //}
 
-function updateStructure(rec1,rec2){
-	//write your code
+const { parse } = require("@babel/core");
+
+function updateStructure(recA, recB) {
+  //write your code
+  if (contains(recA, recB)) {
+    const relativeDim = relative(recA, recB);
+    return { ...recA, children: [relativeDim] };
+  } else if (contains(recB, recA)) {
+    const relativeDim = relative(recB, recA);
+    return { ...recB, children: [relativeDim] };
+  } else {
+    return { ...recA };
+  }
+}
+function contains(recA, recB) {
+  const recAn = normalize(recA);
+  const recBn = normalize(recB);
+  if (
+    recAn.x1 <= recBn.x1 &&
+    recAn.y1 <= recBn.y1 &&
+    recAn.x2 <= recBn.x2 &&
+    recAn.y2 <= recBn.y2
+  ) {
+    return true;
+  }
+  return false;
+}
+function relative(recA, recB) {
+  const recAn = normalize(recA);
+  const recBn = normalize(recB);
+
+  const res = { children: recB.children };
+
+  if (recB.top) {
+    res.top = `${recBn.x1 - recAn.x1}px`;
+  }
+  if (recB.left) {
+    res.left = `${recBn.y1 - recAn.y1}px`;
+  }
+
+  if (recB.height) {
+    res.height = recB.height;
+  }
+
+  if (recB.width) {
+    res.width = recB.width;
+  }
+
+  if (recB.bottom) {
+    res.bottom = `${recBn.x2 - recAn.x2}px`;
+  }
+
+  if (recB.right) {
+    res.right = `${recBn.y2 - recAn.y2}px`;
+  }
+  return res;
+}
+const T = 100000;
+const W = 100000;
+function normalize(rec) {
+  return {
+    x1: rec.top
+      ? parseInt(rec.top)
+      : T - (parseInt(rec.bottom) + parseInt(rec.height)),
+    y1: rec.left
+      ? parseInt(rec.left)
+      : W - (parseInt(rec.right) + parseInt(rec.width)),
+    x2: rec.bottom
+      ? parseInt(rec.bottom)
+      : parseInt(rec.top) + parseInt(rec.height),
+    y2: rec.right
+      ? parseInt(rec.right)
+      : parseInt(rec.left) + parseInt(rec.height),
+  };
 }
 
 module.exports = updateStructure;
